b52b248fa3c8f2358f7d7bd65baa8e67ba04a563
Ana-Biscalchin <anacarolinabis@gmail.com>
2019-02-20 21:04:42 -0300

    0.0.6 js funciona

--- script.js
+++ script.js
@@ -1,86 +1,69 @@
-function disabledButton() {
-    document.getElementById("tweet").disabled = true;
-}
+let tweetBtn = document.getElementById("tweet");
+let counter = document.getElementById("counter");
+let textArea = document.getElementById("text_box")
 
-document.getElementById("text_box").addEventListener("keypress", verifica);
+tweetBtn.addEventListener("click", pegarMsg);
+textArea.addEventListener("keydown", count);
+textArea.addEventListener("keydown", verifica);
+textArea.addEventListener("keydown", resize);
+
+function message() {
+    let msg = document.getElementById("text_box").value;
+    return msg
+}
 
 function verifica() {
-    let msg = document.getElementById("text_box").value;
-    console.log(msg)
-    console.log(msg.length)
 
-    if (msg.length >= 0) {
-        document.getElementById("tweet").disabled = false;
-    } else if (msg.length > 140) {
-        document.getElementById("tweet").disabled = true;
+    if (message().length >= 0 && message().length <= 140) {
+        tweetBtn.disabled = false;
+    } else {
+        tweetBtn.disabled = true;
     }
-
 }
 
-document.getElementById("tweet").addEventListener("click", pegarMsg);
-
+function publishHour() {
+    let hour = moment().format('LTS');
+    console.log(hour)
+    let newDivHour = document.createElement("div");
+    document.getElementById("publish").appendChild(newDivHour);
+}
 
 function pegarMsg() {
-    let msg = document.getElementById("text_box").value;
-    console.log(msg)
+
     let newDiv = document.createElement("div");
     let newParagraph = document.createElement("p");
-    let text = document.createTextNode(msg);
+    let text = document.createTextNode(message());
 
     newDiv.appendChild(newParagraph);
     newParagraph.appendChild(text);
-    document.getElementById("publish").appendChild(newDiv);
 
+    document.getElementById("publish").appendChild(newDiv);
+    publishHour();
     reset();
 }
 
+function count() {
+    let countFinal = 140 - message().length;
 
-
-document.getElementById("text_box").addEventListener("keyup", count);
-
-
-let countInitial = 140;
-
-function count(counterIntial) {
-
-    let msg = document.getElementById("text_box").value;
-    let countFinal = 140 - msg.length;
-
-    document.getElementById("counter").innerHTML = countFinal;
-
-    console.log("contador", countFinal)
-
-    if (countFinal < 20 & countFinal > 10) {
-        document.getElementById("counter").classList.add("counter-color-blue");
+    if (countFinal <= 140 & countFinal >= 0) {
+        counter.classList.add("counter-color");
+    } else if (countFinal < 20 & countFinal > 10) {
+        counter.classList.add("counter-color-blue");
     } else if (countFinal < 10) {
-
-        document.getElementById("counter").classList.add("counter-color-red");
+        counter.classList.add("counter-color-red");
     }
+    counter.innerHTML = countFinal;
 }
 
 function reset() {
-    document.getElementById("counter").innerHTML = countInitial;
-    document.getElementById("counter").classList.add("counter-color")
+    counter.innerHTML = 140;
+    counter.classList.remove("counter-color-blue", "counter-color-red");
+    counter.classList.add("counter");
+    tweetBtn.disabled = true;
+    textArea.value = "";
 }
 
-
-
-
-/*
-
- for (document.getElementById("text_box").length < 10) {
-        document.getElementById("tweet").style.backgroundColor = "red";
-    }
-
-   document.getElementById("tweet").disableButton = true;
-
- function count() {
-     let max = new Number();
-     max = 10;
-     let atual = document.getElementById("text_area").value;
-     let valor = max - atual;
-     document.getElementById()
- }
-
- white-space: pre-wrap ------------- Para mudar a cor do contador document.getElementById.style.color = "red"
-  */
+function resize() {
+    textArea.style.cssText = 'height:auto; padding:0';
+    textArea.style.cssText = 'height:' + textArea.scrollHeight + 'px';
+}
